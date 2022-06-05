037ee0000963dda17bc0fc614708de057c1dac86
BuildTools <unconfigured@null.spigotmc.org>
2022-06-04 20:04:43 -0500

    Initial commit

---
+++ src/main/java/net/airclaw/airclawmod/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.airclaw.airclawmod.mixin;
+
+import net.airclaw.airclawmod.AirclawMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		AirclawMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
